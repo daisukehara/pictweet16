f1459991fd38d24d630f509f620db4b279b8befb
daisukehara <hara.dsk.usa@gmail.com>
2016-08-07 12:46:13 +0900

    最低限のindex→new→createはできることを確認（最初のrootの設定がおかしかったので、一度削除した）

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,24 +1,39 @@
 class TweetsController < ApplicationController
 
   def index
+    @tweets = Tweet.order("created_at DESC").page(params[:page]).per(5)
   end
 
   def new
   end
 
   def create
+    Tweet.create(tweet_params)
   end
 
   def edit
+    tweet = Tweet.find(params[:id])
   end
 
   def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params) if user_signed_in? && current_user.id = tweet.user_id
   end
 
   def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
   end
 
   def show
+    tweet = Tweet.find(params[:id])
+  end
+
+  private
+
+
+  def tweet_params
+    params.permit(:text, :image).merge(user_id: current_user.id)
   end
 
   
